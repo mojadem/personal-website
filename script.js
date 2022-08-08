7d612033e83b0d5c90c2b9b8e004d5208b34ed27
matt dembiczak <dembiczakmatt@gmail.com>
2022-08-08 02:18:04 -0400

    about icon menu working

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-document.querySelectorAll(".btn").forEach((el) =>
+var btns = document.querySelectorAll(".btn");
 
-    el.addEventListener("click", () => {
+btns.forEach((btn) =>
 
-        // var id = el.dataset.food;
+    btn.addEventListener("click", () => {
 
-        // var target = document.getElementById(id);
-
-        // target.classList.toggle("visible");
-
-        if (el.classList.contains("active")) {
-            el.classList.remove("active");
+        if (btn.classList.contains("active")) {
+            deactivate(btn);
+            document.getElementById("info-viewport").classList.remove("active");
+            btns.forEach((otherBtn) => {
+                otherBtn.classList.remove("inactive");
+            })
         } else {
-            el.classList.add("active");
+            activate(btn);
+            document.getElementById("info-viewport").classList.add("active");
+            btns.forEach((otherBtn) => {
+                if (!btn.isSameNode(otherBtn)) {
+                    deactivate(otherBtn);
+                }
+            })
         }
     })
 );
+
+function activate(el) {
+    var id = el.dataset.topic;
+    var target = document.getElementById(id);
+    target.classList.add("visible");
+    el.classList.add("active");
+    el.classList.remove("inactive");
+}
+
+function deactivate(el) {
+    var id = el.dataset.topic;
+    var target = document.getElementById(id);
+    target.classList.remove("visible");
+    el.classList.remove("active");
+    el.classList.add("inactive");
+}
