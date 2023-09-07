89a9499416b1c593bf37a9b3173ff868e61ae52c
Matt Dembiczak <dembiczakmatt@gmail.com>
2023-09-07 11:06:56 -0400

    formatting and removed old images

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
 var btns = document.querySelectorAll(".btn");
 
 btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("active")) {
+      var id = btn.dataset.topic;
+      var target = document.getElementById(id);
 
-    btn.addEventListener("click", () => {
+      deactivate(btn);
+      target.parentNode.classList.remove("active"); // deactivate info-viewport
 
-        if (btn.classList.contains("active")) {
-            var id = btn.dataset.topic;
-            var target = document.getElementById(id);
-
-            deactivate(btn);
-            target.parentNode.classList.remove("active"); // deactivate info-viewport
-
-            btns.forEach((otherBtn) => {
-                if (btn.parentNode.isSameNode(otherBtn.parentNode)) {
-                    otherBtn.classList.remove("inactive");
-                }
-            })
-        } else {
-            var id = btn.dataset.topic;
-            var target = document.getElementById(id);
-
-            activate(btn);
-            target.parentNode.classList.add("active"); // activate info-viewport
-
-            btns.forEach((otherBtn) => {
-                if (!btn.isSameNode(otherBtn) && btn.parentNode.isSameNode(otherBtn.parentNode)) {
-                    deactivate(otherBtn);
-                }
-            })
+      btns.forEach((otherBtn) => {
+        if (btn.parentNode.isSameNode(otherBtn.parentNode)) {
+          otherBtn.classList.remove("inactive");
         }
-    })
+      });
+    } else {
+      var id = btn.dataset.topic;
+      var target = document.getElementById(id);
+
+      activate(btn);
+      target.parentNode.classList.add("active"); // activate info-viewport
+
+      btns.forEach((otherBtn) => {
+        if (
+          !btn.isSameNode(otherBtn) &&
+          btn.parentNode.isSameNode(otherBtn.parentNode)
+        ) {
+          deactivate(otherBtn);
+        }
+      });
+    }
+  }),
 );
 
 function activate(el) {
-    var id = el.dataset.topic;
-    var target = document.getElementById(id);
-    target.classList.add("visible");
-    el.classList.add("active");
-    el.classList.remove("inactive");
+  var id = el.dataset.topic;
+  var target = document.getElementById(id);
+  target.classList.add("visible");
+  el.classList.add("active");
+  el.classList.remove("inactive");
 }
 
 function deactivate(el) {
-    var id = el.dataset.topic;
-    var target = document.getElementById(id);
-    target.classList.remove("visible");
-    el.classList.remove("active");
-    el.classList.add("inactive");
+  var id = el.dataset.topic;
+  var target = document.getElementById(id);
+  target.classList.remove("visible");
+  el.classList.remove("active");
+  el.classList.add("inactive");
 }
